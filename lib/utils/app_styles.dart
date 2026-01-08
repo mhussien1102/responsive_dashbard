9beda7465c49fa06bd23748fce54b928a27f6bac
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2026-01-08 15:20:31 +0200

    55.add styles

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -1,5 +1,69 @@
 import 'dart:ui';
 
 abstract class AppStyles {
-  TextStyle font16w600 = TextStyle(fontSize: 16, fontWeight: FontWeight.w600);
+  static final TextStyle styleRegular16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'MontSerrat',
+    color: Color(0xff064060),
+  );
+  static final TextStyle styleMedium16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'MontSerrat',
+    color: Color(0xff064060),
+  );
+  static final TextStyle styleSemiBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'MontSerrat',
+    color: Color(0xff064060),
+  );
+  static final TextStyle styleBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w700,
+    fontFamily: 'MontSerrat',
+    color: Color(0xff4eb7f2),
+  );
+
+  static final TextStyle styleSemiBold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'MontSerrat',
+    color: Color(0xff064060),
+  );
+  static final TextStyle styleRegular12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'MontSerrat',
+    color: Color(0xffaaaaaa),
+  );
+
+  static final TextStyle styleSemiBold24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'MontSerrat',
+    color: Color(0xff4eb7f2),
+  );
+
+  static final TextStyle styleRegular14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'MontSerrat',
+    color: Color(0xffaaaaaa),
+  );
+
+  static final TextStyle styleSemiBold18 = TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'MontSerrat',
+    color: Color(0xffffffff),
+  );
+
+  static final TextStyle styleMedium20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'MontSerrat',
+    color: Color(0xffffffff),
+  );
 }
