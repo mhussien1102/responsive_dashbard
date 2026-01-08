13662b708756d6a3e9fba2177d5d796d07739d09
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2026-01-08 13:36:10 +0200

    54.style Organizer

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+abstract class AppStyles {
+  TextStyle font16w600 = TextStyle(fontSize: 16, fontWeight: FontWeight.w600);
+}
--- lib/views/dashboard_views.dart
+++ lib/views/dashboard_views.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashbard/views/widgets/adaptive_layout.dart';
+import 'package:responsive_dashbard/views/widgets/dashboard_desktop_layout.dart';
 
 class DashboardViews extends StatelessWidget {
   const DashboardViews({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => SizedBox(),
+        tabletLayout: (context) => SizedBox(),
+        desktopLayout: (context) => DashboardDesktopLayout(),
+      ),
+    );
   }
 }
