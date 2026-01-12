1aa0ded33a61bb5ed39c0a8d15713be7f22507f6
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2026-01-12 15:45:36 +0200

    61.fix issues in the text style

--- lib/utils/app_styles.dart
+++ lib/utils/app_styles.dart
@@ -4,66 +4,66 @@ abstract class AppStyles {
   static final TextStyle styleRegular16 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w400,
-    fontFamily: 'MontSerrat',
+    fontFamily: 'Montserrat',
     color: Color(0xff064060),
   );
   static final TextStyle styleMedium16 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w500,
-    fontFamily: 'MontSerrat',
+    fontFamily: 'Montserrat',
     color: Color(0xff064060),
   );
   static final TextStyle styleSemiBold16 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w600,
-    fontFamily: 'MontSerrat',
+    fontFamily: 'Montserrat',
     color: Color(0xff064060),
   );
   static final TextStyle styleBold16 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w700,
-    fontFamily: 'MontSerrat',
+    fontFamily: 'Montserrat',
     color: Color(0xff4eb7f2),
   );
 
   static final TextStyle styleSemiBold20 = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.w600,
-    fontFamily: 'MontSerrat',
+    fontFamily: 'Montserrat',
     color: Color(0xff064060),
   );
   static final TextStyle styleRegular12 = TextStyle(
     fontSize: 12,
     fontWeight: FontWeight.w400,
-    fontFamily: 'MontSerrat',
+    fontFamily: 'Montserrat',
     color: Color(0xffaaaaaa),
   );
 
   static final TextStyle styleSemiBold24 = TextStyle(
     fontSize: 24,
     fontWeight: FontWeight.w600,
-    fontFamily: 'MontSerrat',
+    fontFamily: 'Montserrat',
     color: Color(0xff4eb7f2),
   );
 
   static final TextStyle styleRegular14 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w400,
-    fontFamily: 'MontSerrat',
+    fontFamily: 'Montserrat',
     color: Color(0xffaaaaaa),
   );
 
   static final TextStyle styleSemiBold18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600,
-    fontFamily: 'MontSerrat',
+    fontFamily: 'Montserrat',
     color: Color(0xffffffff),
   );
 
   static final TextStyle styleMedium20 = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.w500,
-    fontFamily: 'MontSerrat',
+    fontFamily: 'Montserrat',
     color: Color(0xffffffff),
   );
 }
