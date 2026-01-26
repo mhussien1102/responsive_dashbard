69a416e99116511f8388feaa54196cb471a2746c
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2026-01-26 11:16:32 +0200

    74.all_expensess_item_list_view_part1

---
+++ lib/views/widgets/all_expensess_item_list_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dashbard/models/all_expensess_item_model.dart';
+import 'package:responsive_dashbard/views/widgets/all_expensess_item.dart';
+
+import '../../utils/app_images.dart';
+
+class AllExpensessItemListView extends StatelessWidget {
+  const AllExpensessItemListView({super.key});
+
+  static const items = [
+    AllExpensessItemModel(
+      img: Assets.imagesIncome,
+      title: 'Income',
+      data: 'April 2022',
+      price: r'$20,129',
+    ),
+    AllExpensessItemModel(
+      img: Assets.imagesExpenses,
+      title: 'Expenses',
+      data: 'April 2022',
+      price: r'$20,129',
+    ),
+    AllExpensessItemModel(
+      img: Assets.imagesIncome,
+      title: 'Balance',
+      data: 'April 2022',
+      price: r'$20,129',
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, index) {
+        return AllExpensessItem(itemModel: items[index]);
+      },
+    );
+  }
+}
