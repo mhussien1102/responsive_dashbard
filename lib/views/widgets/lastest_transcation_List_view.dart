d27cbac1ee5fd85d2d246b022091c28a9b8fe81b
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2026-02-27 15:12:40 +0200

    88.Last transcation done

--- lib/views/widgets/lastest_transcation_List_view.dart
+++ lib/views/widgets/lastest_transcation_List_view.dart
@@ -26,16 +26,14 @@ class LastestTranscationListView extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 80,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          return IntrinsicWidth(
-            child: UserInfoListTile(userInfoModel: items[index]),
-          );
-        },
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: items
+            .map(
+              (e) => IntrinsicWidth(child: UserInfoListTile(userInfoModel: e)),
+            )
+            .toList(),
       ),
     );
   }
