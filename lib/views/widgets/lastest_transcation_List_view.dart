9176a488af1373de78684ef9c63b33882cc54144
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2026-02-27 14:37:47 +0200

    86.Intrinsic Width

--- lib/views/widgets/lastest_transcation_List_view.dart
+++ lib/views/widgets/lastest_transcation_List_view.dart
@@ -26,12 +26,17 @@ class LastestTranscationListView extends StatelessWidget {
   ];
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      scrollDirection: Axis.horizontal,
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        return UserInfoListTile(userInfoModel: items[index]);
-      },
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return IntrinsicWidth(
+            child: UserInfoListTile(userInfoModel: items[index]),
+          );
+        },
+      ),
     );
   }
 }
--- lib/views/widgets/quick_invoice.dart
+++ lib/views/widgets/quick_invoice.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashbard/views/widgets/custom_background_container.dart';
+import 'package:responsive_dashbard/views/widgets/lastest_transcation_List_view.dart';
 import 'package:responsive_dashbard/views/widgets/quick_invoice_header.dart';
 
 class QuickInvoice extends StatelessWidget {
@@ -9,7 +10,9 @@ class QuickInvoice extends StatelessWidget {
   Widget build(BuildContext context) {
     return CustomBackgroundContainer(
       padding: 24,
-      child: Column(children: [QuickInvoiceHeader()]),
+      child: Column(
+        children: [QuickInvoiceHeader(), LastestTranscationListView()],
+      ),
     );
   }
 }
