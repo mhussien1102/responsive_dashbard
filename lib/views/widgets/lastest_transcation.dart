de139b95941f96e8af623a82902790acbd5a1967
mohammedhussien <mohammed403774@fci.bu.edu.eg>
2026-02-27 16:29:33 +0200

    89.custom text field

--- lib/views/widgets/lastest_transcation.dart
+++ lib/views/widgets/lastest_transcation.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashbard/utils/app_styles.dart';
+import 'package:responsive_dashbard/views/widgets/lastest_transcation_List_view.dart';
 
 class LastestTransciation extends StatelessWidget {
   const LastestTransciation({super.key});
@@ -7,12 +8,14 @@ class LastestTransciation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           'Latest Transaction',
           style: AppStyles.styleMedium16.copyWith(color: Color(0xff064061)),
         ),
         SizedBox(height: 12),
+        LastestTranscationListView(),
       ],
     );
   }
--- lib/views/widgets/quick_invoice.dart
+++ lib/views/widgets/quick_invoice.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashbard/views/widgets/custom_background_container.dart';
+import 'package:responsive_dashbard/views/widgets/lastest_transcation.dart';
 import 'package:responsive_dashbard/views/widgets/lastest_transcation_List_view.dart';
 import 'package:responsive_dashbard/views/widgets/quick_invoice_header.dart';
+
+import 'custom_text_field.dart';
 
 class QuickInvoice extends StatelessWidget {
   const QuickInvoice({super.key});
@@ -11,7 +14,13 @@ class QuickInvoice extends StatelessWidget {
     return CustomBackgroundContainer(
       padding: 24,
       child: Column(
-        children: [QuickInvoiceHeader(), LastestTranscationListView()],
+        children: [
+          QuickInvoiceHeader(),
+          LastestTransciation(),
+
+          Divider(height: 48),
+          CustomTextField(),
+        ],
       ),
     );
   }
